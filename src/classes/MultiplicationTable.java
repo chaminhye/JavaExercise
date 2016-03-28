f2164a0c014b3ce4f5cfbe25dfc06cfa3d9d1dc5
chaminhye <minhye_love@naver.com>
2016-03-28 14:50:47 +0900

    Update MultiplicationTable.java

--- src/classes/MultiplicationTable.java
+++ src/classes/MultiplicationTable.java
@@ -1,4 +1,4 @@
-package classes;
+
 
 /**
  * Created by jyheo on 2016-03-26.
@@ -38,10 +38,12 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         MultiplicationTable mtable = new MultiplicationTable();
 
-        mtable.max_number = 19; // BAD practice!
-        mtable.setMaxNumber(19);
+        mtable.max_number = 9; // BAD practice!
+        mtable.setMaxNumber(9);
 
-        mtable.setNumber(4);
+        for(int i=2;i<=9;i++)
+        {
+        mtable.setNumber(i);
         mtable.printTable();
-    }
-}
+         }
+}}
